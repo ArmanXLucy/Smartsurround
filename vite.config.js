9b4f0c20f9c021630f81170087f6a5a913fe1c96
ArmanXLucy <arman2klo1967@gmail.com>
2026-08-28 00:23:34 +0530

    Initial SmartSurround project

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+<<<<<<< HEAD
+  plugins: [react()],
+});
+=======
+  plugins: [react(), tailwindcss()],
+});
+>>>>>>> 03a952e (login page created with db and with a minimal bug/poka)
